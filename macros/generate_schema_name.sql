c73d76d480198eae4a30f740a31b4d826e7b6cb5
serhiipuzyrov <61794175+serhiipuzyrov@users.noreply.github.com>
2025-11-28 11:31:19 +0100

    Revert "Dev"

--- macros/generate_schema_name.sql
+++
@@ -1,3 +0,0 @@
-{% macro generate_schema_name(custom_schema_name, node) %}
-    {{ node.fqn[-2] }}
-{% endmacro %}
--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,4 +1,5 @@
-{{ config(materialized='view') }}
+
+-- Use the `ref` function to select from other models
 
 select *
 from {{ ref('my_first_dbt_model') }}
